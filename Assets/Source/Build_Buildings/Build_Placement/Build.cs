237ba2f5f0cac9373b6e3a3295eada309ff999b6
Warhammer2000 <rustam.r2005@gmail.com>
2023-09-04 02:49:18 +0500

    fix are successfuly done

--- Assets/Source/Build_Buildings/Build_Placement/Build.cs
+++ Assets/Source/Build_Buildings/Build_Placement/Build.cs
@@ -9,9 +9,13 @@ public class Build : MonoBehaviour
     public Material matCanBuilded;
     public Material matCantBuilded;
     public Material matBuilded;
+    public Material matNotBuilded;
 
     public Renderer render;
-    public BuildingBrain brain; 
+    public BuildingBrain brain;
+    private bool isSelected;
+    private bool isBuilded;
+
     private void Awake()
     {
         Instance = this;
@@ -24,16 +28,40 @@ public class Build : MonoBehaviour
         matCanBuilded = Resources.Load<Material>("MaterailToLoad/MatCanBuilded");
         matCantBuilded = Resources.Load<Material>("MaterailToLoad/MatCantBuilded");
         matBuilded = Resources.Load<Material>("MaterailToLoad/MatBuilded");
+        matNotBuilded = Resources.Load<Material>("MaterailToLoad/MatNotBuilded 1");
+    }
+    private void Update()
+    {
+        if (!isBuilded) Cheker();
     }
     private void OnMouseDown()
     {
-        if (BuildingPlacement.instance.pushed)
+        if (BuildingPlacement.selectedTowerToBuy && !isBuilded && isSelected)
         {
             Vector3 thistransform = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
-            Instantiate(brain.buildingPrices.data[0].prefab, thistransform, Quaternion.identity);
+            Instantiate(BuildingPlacement.selectedTowerToBuy, thistransform, Quaternion.identity);
             render.material = matBuilded;
+            isBuilded = true;
         }
         else render.material = matCantBuilded;
-        
+    }
+    private void OnMouseEnter()
+    {
+        isSelected = true;
+    }
+    private void OnMouseExit()
+    {
+        isSelected = false;
+    }
+    private void Cheker()
+    {
+        if (BuildingPlacement.selectedTowerToBuy)
+        {
+            if (isSelected) render.material = matCanBuilded;
+            else render.material = matNotBuilded;
+        }
+        else render.material = matCantBuilded;
+
+        if (isBuilded) render.material = matBuilded;
     }
 }
--- Assets/Source/Build_Buildings/Build_Placement/BuildingPlacement.cs
+++ Assets/Source/Build_Buildings/Build_Placement/BuildingPlacement.cs
@@ -2,6 +2,7 @@
 
 public class BuildingPlacement : MonoBehaviour
 {
+    public static GameObject selectedTowerToBuy;
     public static BuildingPlacement instance;
     private BuildingBrain _brain;
     public bool pushed;
@@ -11,16 +12,25 @@ public class BuildingPlacement : MonoBehaviour
         instance = this;
         _brain = GetComponent<BuildingBrain>();
     }
-    public void PCBUILDING()
+    public void PCBUILDING(int index)
     {
          if (_brain.CanBuildBuilding(_brain.buildingPrices))
          {
-            Build.Instance.render.material = Build.Instance.matCanBuilded;
-            pushed = true;
+             switch(index)
+            {
+                case 0: selectedTowerToBuy = _brain.buildingPrices.data[0].prefab; break;
+            }
          }
          else
          {
             Debug.LogWarning("Ты не можешь купить здание");          
          } 
+    }
+    private void LateUpdate()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            selectedTowerToBuy = null;
+        }
     }
 }
